7d7e4b5b8d415f4c8cb96c12e0cd390708deb74a
Ellie Maidment <ellie947@hotmail.com>
2020-09-24 13:31:31 +0100

    add Container max-width md

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
+import Grid from '@material-ui/core/Grid';
 
 function Header() {
   return (
     <div>
-      <Container max-width="sm">
+      <Container max-width="md">
+        <Grid item xs={12}>
         <Typography variant="h3">The Sentimentalists</Typography>
         <Typography variant="h5">Analysing Sentiment Since 2020</Typography>
+        </Grid>
       </Container>
     </div>
   );

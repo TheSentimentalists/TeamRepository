b98043b3d9aafc9def2bb050710498b25eb52003
Ellie Maidment <ellie947@hotmail.com>
2020-09-24 15:03:21 +0100

    add Container fixed to URLInput

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,6 +14,14 @@ function Header() {
         <Typography variant="h3">The Sentimentalists</Typography>
         <Typography variant="h5">Analysing Sentiment Since 2020</Typography>
         </Grid>
+        <Grid item xs={12} md={12}>
+        <Typography variant="body2">
+          Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro.
+          De carne lumbering animata corpora quaeritis. Summus brains sit​​,
+          morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum
+          mauris.
+        </Typography>
+      </Grid>
       </Container>
     </div>
   );
--- src/components/URLInput.js
+++ src/components/URLInput.js
@@ -4,38 +4,11 @@ import Typography from "@material-ui/core/Typography";
 
 function URLInput() {
   return (
-    <Container maxWidth="md">
-      <Grid container
-        direction="row"
-        alignItems="flex-start"
-        justify="center"
-        item
-        xs={12}
-        md={12}
-      >
-        <Typography variant="body2">
-          Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro.
-          De carne lumbering animata corpora quaeritis. Summus brains sit​​,
-          morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum
-          mauris.{" "}
-        </Typography>
-      </Grid>
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={6}>
-          <TextField
-            id="outlined-basic"
-            label="Input URL..."
-            variant="outlined"
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Button variant="contained" color="primary">
-            Analyze
-          </Button>
-        </Grid>
-      </Grid>
-    </Container>
+    <div>
+      <Container fixed>
+            
+      </Container>
+    </div>
   );
 }
 

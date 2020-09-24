e8c7aebc07ec51c3cbe7946cb2f5a68e5c052c8a
Ellie Maidment <ellie947@hotmail.com>
2020-09-24 13:27:29 +0100

    add Container to Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header'
-import URLInput from './components/URLInput'
+import Header from './components/Header';
+import URLInput from './components/URLInput';
+
 
 function App() {
   return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 
 function Header() {
   return (
-    <Typography variant="h3">The Sentimentalists</Typography>
-  ); 
+    <div>
+      <Container max-width="sm">
+        <Typography variant="h3">The Sentimentalists</Typography>
+        <Typography variant="h5">Analysing Sentiment Since 2020</Typography>
+      </Container>
+    </div>
+  );
 }
 
 export default Header;

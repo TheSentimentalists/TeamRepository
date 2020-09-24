f1ac05d10c2c462fba441d2bcdd1682d71be8f86
Ellie Maidment <ellie947@hotmail.com>
2020-09-24 14:02:32 +0100

    Grid with flex attribiutes added to URL input

--- src/components/URLInput.js
+++ src/components/URLInput.js
@@ -1,20 +1,41 @@
-import React from 'react';
-import { TextField, Button, Grid, Container } from '@material-ui/core'
+import React from "react";
+import { TextField, Button, Grid, Container } from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
 
 function URLInput() {
   return (
     <Container maxWidth="md">
-    <Grid container spacing={3}>
-      
-            <Grid item xs={12} md={6}>
-                <TextField id="outlined-basic" label="Input URL..." variant="outlined" fullWidth />
-            </Grid>
-            <Grid item xs={12} md={6}>
-                <Button variant="contained" color="primary">Analyze</Button>
-            </Grid>
-    </Grid>
+      <Grid
+        direction="row"
+        alignItems="flex-start"
+        justify="center"
+        item
+        xs={12}
+        md={12}
+      >
+        <Typography variant="body2">
+          Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro.
+          De carne lumbering animata corpora quaeritis. Summus brains sit​​,
+          morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum
+          mauris.{" "}
+        </Typography>
+      </Grid>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6}>
+          <TextField
+            id="outlined-basic"
+            label="Input URL..."
+            variant="outlined"
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Button variant="contained" color="primary">
+            Analyze
+          </Button>
+        </Grid>
+      </Grid>
     </Container>
-   
   );
 }
 

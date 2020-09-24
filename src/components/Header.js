f3c9dab3a43d368043e7a78bd6b9bd999af23aef
Ellie Maidment <ellie947@hotmail.com>
2020-09-24 14:09:48 +0100

    reformat MaterialUI import

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,11 @@
 import React from "react";
-import Typography from "@material-ui/core/Typography";
-import Container from "@material-ui/core/Container";
-import Grid from '@material-ui/core/Grid';
+import {Typography, Container, Grid }from "@material-ui/core";
+
 
 function Header() {
   return (
     <div>
-      <Container max-width="md">
+      <Container max-width="lg">
         <Grid item xs={12}>
         <Typography variant="h3">The Sentimentalists</Typography>
         <Typography variant="h5">Analysing Sentiment Since 2020</Typography>

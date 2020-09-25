93608705def9a837055331343521419a1ec962df
Ellie Maidment <ellie947@hotmail.com>
2020-09-25 09:39:43 +0100

    useState defined with an array of objects

--- src/components/DisplayText.js
+++ src/components/DisplayText.js
@@ -2,16 +2,22 @@ import React, { useState }  from "react";
 import Typography from '@material-ui/core/Typography';
 
 function DisplayText() {
+    const [ text, setText ] = useState([
+        { text: "Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro." },
+        { text: "De carne lumbering animata corpora quaeritis."},
+        { text: "Summus brains sit​​, morbo vel maleficia?"}
+    ]);/*useState is defining the state of the DisplayText component, the array contains the current state (an array with 3 text objects)
+            The setText is the function that will be used to update the state */
+
+
     return (
     <p>
-    <Typography variant="body2" text="Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro."></Typography>
-    <Typography variant="body2"text="De carne lumbering animata corpora quaeritis."></Typography>
-    <Typography variant="body2" text="Summus brains sit​​, morbo vel maleficia?"></Typography>
+    <Typography variant="body2"></Typography>
+    <Typography variant="body2"></Typography>
+    <Typography variant="body2"></Typography>
     </p>
     )
 
 }
-
-
 
 export default DisplayText;

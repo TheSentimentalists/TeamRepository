20c3451834f30b89ed081165329ca9e58d149745
Ellie Maidment <ellie947@hotmail.com>
2020-09-26 11:39:52 +0100

    add useState to display blurb

--- src/App.js
+++ src/App.js
@@ -4,16 +4,35 @@ import "./App.css";
 import Header from "./components/Header";
 import Text from "./components/Text";
 
-function App() {
-  const text = "test"; //declaring a variable with a value stored in it, the variable is passed to the Text compoent as a prop(like an HTML attribute)
 
-  return (
+function App() {
+  const text = "test"; 
+  //declaring a variable with a value stored in it, the variable is passed to the Text compoent as a prop(like an HTML attribute)
+
+  const name = "Lucy Goosey";
+const greeting = "How are you?";
+ const activity = "Buy some coffee";
+ const colour = "Magenta";
+
+ const [blurb] = useState("Leggings bertelli gucci, work it girl opening strut would her never design.");
+
+
+ return (
     <div className="App">
       <Header></Header>
+
+      <div>
+        <p>Hello {name}</p>
+  <p>This is a {text}</p>
+  <Text displayGreeting={greeting} displayActivity={activity} displayColour={colour}></Text>
+ <p>{blurb}</p>
+      </div>
       
-      <Text displayText={text}></Text>
     </div>
   );
 }
 
 export default App;
+/*variables name and text are state that is being accessed directly in App. The values are passed directly to some JSX, 
+bypassing any child components. blurb useState is doing exactly the same as this.
+greeting is passed as a prop to Text which renders it in Text component, which is rendered in App*/
--- src/components/Text.js
+++ src/components/Text.js
@@ -3,10 +3,21 @@ import Typography from '@material-ui/core/Typography';
 
 function Text(props){
  return (
- <p>This should display the word: {props.displayText}</p>
+<div>
+ <Typography variant="body2">{props.displayGreeting}<br/>
+ Now it's time to {props.displayActivity}<br/>
+ Your colour of the day is {props.displayColour}
+ </Typography>
+
+ </div>
  )
 }
 
+/*The prop displayGreeting is the the state being passed to the Text component
+App is the parent, text is the child
+Text is reading the state contained in App
+It will not change the prop
+The props word can be used for any prop*/
 
 
 

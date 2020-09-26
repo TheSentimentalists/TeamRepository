fb0147fa289fbc0577ca96ea724a9ab7c420dde6
Ellie Maidment <ellie947@hotmail.com>
2020-09-26 11:00:41 +0100

    variable passed as prop to Text compont

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
+import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
-import URLInput from "./components/URLInput";
-import Footer from "./components/Footer";
-
+import Text from "./components/Text";
 
 function App() {
+  const text = "test"; //declaring a variable with a value stored in it, the variable is passed to the Text compoent as a prop(like an HTML attribute)
 
   return (
     <div className="App">
-      <Header />
-      <URLInput />
-      <Footer />
+      <Header></Header>
+      
+      <Text displayText={text}></Text>
     </div>
   );
 }
